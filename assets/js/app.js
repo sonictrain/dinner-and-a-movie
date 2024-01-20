8177fea14006849f87629dec1a8665c4eb4509b3
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-20 23:09:26 +0000

    change html search input groups, add event listener on the movie search button, organize code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,14 @@
-// $('#search-button').click(async (e) => {
-//     e.preventDefault();
-//     // const searchTerm = $('#search-term').val();
-//     const searchTerm = "titanic";
-//     const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
-//     console.log(searchUrl);
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
-//         }
-//     }
-//     getMovie(searchUrl, options);
-// })
+$(function() {
+    $('#movie-search').click(async (e) => {
+        e.preventDefault();
+        const searchTerm = $('#movie-keyword').val();
+        getMovies(searchTerm);
+    })
+})
 
-
-getMovies();
-
-async function getMovies() {
-    const searchTerm = "titanic";
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
+// ---- FUNCTION TO GET MOVIES FROM FETCHED DATA
+async function getMovies(keyword) {
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=en-US&page=1`;
     const options = {
         method: 'GET',
         headers: {
@@ -27,16 +16,21 @@ async function getMovies() {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
         }
     }
-    const results = await axios.get(searchUrl, options);
-    const movies = results.data.results;
-    console.log(movies);
-    createCard(movies);
+    try {
+        const results = await axios.get(searchUrl, options);
+        const movies = results.data.results;
+        createCard(movies);
+    } catch(err) {
+        console.log("Error with MOVIE search", err);
+    }
 }
 
-
+// ------ FUNCTION TO CREATE CARDS WITH MOVIES' INFO -------
 const createCard = (movies) => {
-    // TODO: clear the search field
-    // $('#search-term').val('');
+    // clear the search field
+    $('#movie-keyword').val('');
+    // clear previous results
+    $('#movie-results').empty();
     // get fetched data for each movie, create a card, add to search results
     $.each(movies, (i, movie) => {
         const poster = getImage(movie.poster_path);
@@ -62,10 +56,11 @@ const createCard = (movies) => {
         const cardBody = $('<div>').addClass('card-body').append(title, releaseDate, rating, desBtn, descDiv);
         const newCard = $('<div>').addClass('card').css({width: '15rem', height: 'auto'});
         newCard.append(poster, cardBody);
-        $('#movie-search').append(newCard);
+        $('#movie-results').append(newCard);
     })
 }
 
+// ---- AUXILIARY FUNCTION TO GET POSTER IMAGE --- 
 function getImage(path) {
     if (!path) {
         imageUrl = "assets/images/movie-poster-not-found.png";
@@ -76,6 +71,7 @@ function getImage(path) {
     return image;
 }
 
+// ----- AUXILIARY FUNCTION TO EXTRACT THE RELEASE YEAR ------
 function getReleaseYear(date) {
     const dateArr = date.split('-');
     const yearOnly = dateArr[0];
