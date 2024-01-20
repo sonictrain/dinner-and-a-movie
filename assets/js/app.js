3ecda8efa7bfcf73e1ad25655b81bff8f99279ec
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-20 21:00:40 +0000

    add basic card with movie poster and info

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,39 +15,72 @@
 // })
 
 
+getMovies();
 
-const searchTerm = "titanic";
-const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
-console.log(searchUrl);
-
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
+async function getMovies() {
+    const searchTerm = "titanic";
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
+    // ! fetch url for list of movies
+    console.log(searchUrl);
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
+        }
     }
+    const results = await axios.get(searchUrl, options);
+    const movies = results.data.results;
+    // ! list of movies
+    console.log(movies);
+    createCard(movies);
 }
-getMovie(searchUrl, options);
 
 
+const createCard = (movies) => {
+    // TODO: clear the search field
+    // $('#search-term').val('');
+    // get fetched data for each movie, create a card, add to search results
+    $.each(movies, (i, movie) => {
+        let poster = getImage(movie);
+        console.log(poster);
+        let title = $('<h5>').text(movie.title).addClass('card-title');
+        let releaseDate = $('<p>').text(movie.release_date);
+        let rating = $('<p>').text(movie.vote_average.toFixed(1));
+        // collapsable button for descriptoin
+        const desBtn = $('<button>')
+        .addClass('btn btn-outline-secondary btn-sm mx-1 mb-2')
+        .attr('type', 'button')
+        .attr('data-bs-toggle', 'collapse')
+        .attr('data-bs-target', '#collapseDesc')
+        .attr('aria-expanded', 'false')
+        .attr('aria-controls', 'collapseDesc')
+        .text('Description');
+        let description = $('<p>').html(movie.overview).addClass('desc');
+        const descInnerCard = $('<div>').addClass('card card-body').append(description);
+        const descDiv = $('<div>').addClass('collapse').attr('id', 'collapseDesc');
+        descDiv.append(descInnerCard);
+        const cardBody = $('<div>').addClass('card-body').append(title, releaseDate, rating, desBtn);
+        const newCard = $('<div>').addClass('card').css({width: '15rem', height: 'auto'});
+        newCard.append(poster, cardBody);
+        $('#movie-search').prepend(newCard);
+    })
+}
 
-async function getMovie(url, config) {
-    try {
-        const res = await fetch(url, config);
-        const data = await res.json();
-        console.log(data);
-        const movies = data.results;
-        console.log(movies);
-        const title = $('<h4>').text(data.results[0].title);
-        const releaseDate = $('<p>').text(data.results[0].release_date);
-        const description = $('<p>').text(data.results[0].overview);
-        const imageFile = data.results[0].poster_path;
-        const imageUrl = `https://image.tmdb.org/t/p/w154${imageFile}`;
-        const imgEl = $('<img>').attr('src', imageUrl);
+function getImage(item) {
+    let imageFile = item.poster_path;
+    // console.log(imageFile);
+    if (!imageFile) {
+        // imageUrl = 'https://placehold.co/154x231';
+        imageUrl = "assets/images/movie-poster-not-found.png";
+    } else {
+        imageUrl = `https://image.tmdb.org/t/p/w154${imageFile}`;
+    }
+    // console.log(imageUrl);
+    let image = $('<img>').attr('src', imageUrl).addClass('card-img-top');
+    return image;
+}
+
         // const movieCard = ('<div>').addClass('card').append(imgEl, title, releaseDate, description);
         // $('.movies').append(movieCard);
-    } catch(err) {
-        console.error("Error with API", err)
-    }
-}
-
+   
