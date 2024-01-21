cd8ca02b3efacdc93c66554dce544b9bbd79f9c4
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-21 12:08:57 +0000

    fetch another API option to extract a link with viewing options in GB and open it in a new tab

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,4 @@
 $(function() {
-    $('#movie-search').click(async (e) => {
-        e.preventDefault();
-        const searchTerm = $('#movie-keyword').val();
-        getMovies(searchTerm);
-    })
-})
-
-// ---- FUNCTION TO GET MOVIES FROM FETCHED DATA
-async function getMovies(keyword) {
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=en-US&page=1`;
     const options = {
         method: 'GET',
         headers: {
@@ -16,6 +6,16 @@ async function getMovies(keyword) {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
         }
     }
+    $('#movie-search').click(async (e) => {
+        e.preventDefault();
+        const searchTerm = $('#movie-keyword').val();
+        getMovies(searchTerm, options);
+    })
+})
+
+// ---- FUNCTION TO GET MOVIES FROM FETCHED DATA
+async function getMovies(keyword, options) {
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=en-US&page=1`;
     try {
         const results = await axios.get(searchUrl, options);
         const movies = results.data.results;
@@ -23,6 +23,7 @@ async function getMovies(keyword) {
         createCard(movies);
     } catch(err) {
         console.log("Error with MOVIE search", err);
+        // TODO: add a modal with error message
     }
 }
 
@@ -34,6 +35,7 @@ const createCard = (movies) => {
     $('#movie-results').empty();
     // get fetched data for each movie, create a card, add to search results
     $.each(movies, (i, movie) => {
+        const movieID = movie.id;
         const poster = getImage(movie.poster_path);
         const title = $('<h5>').text(movie.title).addClass('card-title');
         const year = getReleaseYear(movie.release_date);
@@ -44,7 +46,7 @@ const createCard = (movies) => {
             rating = $('<p>').text('N/A');
         };
         const desBtn = $('<button>')
-            .addClass('btn btn-outline-secondary mx-1 mb-2')
+            .addClass('btn btn-outline-secondary btn-md mx-1 mb-2')
             .attr('type', 'button')
             .attr('data-bs-toggle', 'collapse')
             .attr('data-bs-target', '#collapseDesc')
@@ -62,12 +64,63 @@ const createCard = (movies) => {
         // create the lower div for description and attach the inner div
         const descDiv = $('<div>').addClass('collapse').attr('id', 'collapseDesc');
         descDiv.append(descInnerCard);
-        const cardBody = $('<div>').addClass('card-body').append(title, releaseDate, rating, desBtn, descDiv);
+        const providersBtn = $('<button>')
+            .addClass('btn btn-primary providersBtn')
+            .attr('data-movieID', movieID)
+            .text('Watch');
+        const cardBody = $('<div>').addClass('card-body').append(title, releaseDate, rating, desBtn, descDiv, providersBtn);
         const newCard = $('<div>').addClass('card').css({width: '15rem', height: 'auto'});
         newCard.append(poster, cardBody);
         $('#movie-results').append(newCard);
     })
 }
+
+$(document).on('click', '.providersBtn', function(options) {
+    const thisMovieID = $(this).data('movieid');
+    console.log(thisMovieID);
+    getMovieLink(thisMovieID);
+})
+
+// ----- FUNCTION TO FETCH THE WATCH PROVIDERS ------
+async function getMovieLink(id) {
+    try {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGVlNGY1NTQ0ZDA5NGYxZmYyZWE2MWU3YzlkMGFjYSIsInN1YiI6IjY1YTk5MzQxYzRhZDU5MDBjYjk2NTE2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.62LSjglzjwChTtYhCfgruCqPBs1Dfk1mGoEi-pqkV6A'
+            }
+        }
+        // fetch(`https://api.themoviedb.org/3/movie/${id}/watch/providers`, options)
+        //     .then(response => response.json())
+        //     .then(response => console.log(response))
+        //     .catch(err => console.error(err));
+
+
+        const results = await axios.get(`https://api.themoviedb.org/3/movie/${id}/watch/providers`, options);
+        console.log(results.data.results);
+        const movieLinkGB = results.data.results.GB;
+        if (!movieLinkGB) {
+            // TODO: change into a modal
+            alert("Not available in the UK or no link found");
+        } else {
+            // console.log(movieLinkGB);
+            const movieLink = movieLinkGB.link;
+            console.log(movieLink);
+            var newTab = window.open(movieLink, '_blank');
+            newTab.focus();
+    //         // window.location.href = movieLink;
+        }
+    } catch(err) {
+        console.log("Error with PROVIDERS search", err);
+        // TODO: change int a modal with error message
+        alert("No link found");
+    }
+}
+
+
+
+
 
 // ---- AUXILIARY FUNCTION TO GET POSTER IMAGE --- 
 function getImage(path) {
